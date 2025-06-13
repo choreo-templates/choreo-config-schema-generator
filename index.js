a22bebee8ad11510a2433495d132fec938f7729b
Janaka Sandaruwan <janakasandaruwan1996@gmail.com>
2025-06-13 17:59:21 +0530

    Skip file creation only if configForm related configs are not defined

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function main() {
       })
     }
 
-    if (configs.env.length === 0 && configs.file.length === 0) {
+    if (schema.length === 0) {
       console.log("No configurations found in the component.yaml");
       return;
     }
